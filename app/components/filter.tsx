4750df42914ea5af9c5c4e5e90c2de8b74adaadb
ozturk-murat <muratoztuurk@gmail.com>
2023-04-13 01:30:26 +0300

    filtered data bug fixed

--- app/components/filter.tsx
+++ app/components/filter.tsx
@@ -10,83 +10,50 @@ import styles from "../../styles/header.module.css";
 
 const inter = JetBrains_Mono({ subsets: ["latin"] });
 
-const people = [
-  {
-    id: 1,
-    value: 1,
-    magnitude: "0 & above",
-  },
-  {
-    id: 2,
-    value: 2,
-    magnitude: "1 & above",
-  },
-  {
-    id: 3,
-    value: 3,
-    magnitude: "2 & above",
-  },
-  {
-    id: 4,
-    value: 4,
-    magnitude: "3 & above",
-  },
-  {
-    id: 5,
-    value: 5,
-    magnitude: "4 & above",
-  },
-  {
-    id: 6,
-    value: 6,
-    magnitude: "5 & above",
-  },
-  {
-    id: 7,
-    value: 7,
-    magnitude: "6 & above",
-  },
-  {
-    id: 8,
-    value: 8,
-    magnitude: "7 & above",
-  },
-  {
-    id: 9,
-    value: 9,
-    magnitude: "8 & above",
-  },
-  {
-    id: 10,
-    value: 10,
-    magnitude: "9 & above",
-  },
-  {
-    id: 11,
-    value: 11,
-    magnitude: "10 & above",
-  },
+export interface Magnitude {
+  id: number;
+  value: number;
+}
+
+const numbers = [
+  { id: 1, value: 0 },
+  { id: 2, value: 1 },
+  { id: 3, value: 2 },
+  { id: 4, value: 3 },
+  { id: 5, value: 4 },
+  { id: 6, value: 5 },
+  { id: 7, value: 6 },
+  { id: 8, value: 7 },
+  { id: 9, value: 8 },
+  { id: 10, value: 9 },
+  { id: 11, value: 10 },
 ];
+
 function classNames(...classes: [any]) {
   return classes.filter(Boolean).join(" ");
 }
 
-interface Props {
-  onFilterChange: (selectedValue: number) => void
+type Number = {
+  id: number;
+  value: number;
+  magnitude: string;
 }
 
-export default function Filter({ onFilterChange }: Props) {
-  const [selected, setSelected] = useState<number>(0);
+interface FilterProps {
+  setSelectedMagnitude: React.Dispatch<React.SetStateAction<Magnitude>>;
+}
 
-  const handleSelection = (value: number) => {
-    setSelected(value)
-    onFilterChange(value)
-  }
+export default function Filter({ setSelectedMagnitude }: FilterProps) {
+  const [selectedNumber, setSelectedNumber] = useState(0)
+
+  const handleChange = (value: { id: number, value: number }) => {
+    setSelectedMagnitude(value);
+  };
 
   return (
     <div className="flex container mt-10">
       <div className="flex flex-col lg:w-3/12 justify-start">
-        <Listbox value={selected} onChange={handleSelection}>
+        <Listbox value={null} onChange={handleChange}>
           {({ open }) => (
             <div className="flex flex-col w-full">
               <Listbox.Label className="block text-sm w-full font-medium leading-10 text-gray-900">
@@ -96,7 +63,7 @@ export default function Filter({ onFilterChange }: Props) {
                 <Listbox.Button className="w-full h-12 cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
                   <span className="flex items-center">
                     <span className="ml-3 block truncate">
-                      {selected}
+                      {null}
                     </span>
                   </span>
                   <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
@@ -114,9 +81,9 @@ export default function Filter({ onFilterChange }: Props) {
                   leaveTo="opacity-0"
                 >
                   <Listbox.Options className="absolute w-50 z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {people.map((magnitude) => (
+                    {numbers.map((mag) => (
                       <Listbox.Option
-                        key={magnitude.id}
+                        key={mag.id}
                         className={({ active }) =>
                           classNames(
                             active
@@ -124,7 +91,7 @@ export default function Filter({ onFilterChange }: Props) {
                               : "text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9"
                           )
                         }
-                        value={magnitude}
+                        value={mag}
                       >
                         {({ selected, active }) => (
                           <>
@@ -136,7 +103,7 @@ export default function Filter({ onFilterChange }: Props) {
                                     : "font-normal ml-3 block truncate"
                                 )}
                               >
-                                {magnitude.magnitude}
+                                {mag.value}
                               </span>
                             </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Filter from "./components/filter";
 import { getEarthquakes, Earthquake } from "./api/earthquakeList";
 import Spinner from "./components/spinner";
-import classNames from "classnames";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,24 +12,37 @@ interface FilterProps {
   setFilteredData: React.Dispatch<React.SetStateAction<Earthquake[]>>;
 }
 
+interface Magnitude {
+  id: number;
+  value: number;
+}
+
 export default function Home(props: EarthquakeListProps) {
   const [earthquakes, setEarthquakes] = useState<Earthquake[]>([]);
   const [filteredData, setFilteredData] = useState<Earthquake[]>([]);
+  const [selectedMagnitude, setSelectedMagnitude] = useState<Magnitude>({ id: 1, value: 0 });
+  const [filteredDataValue, setFilteredDataValue] = useState(0)
   const [loading, setLoading] = useState(true);
-  const [color, setColor] = useState("bg-orange-500");
 
   useEffect(() => {
     getEarthquakes().then((earthquakes) => {
       setEarthquakes(earthquakes);
       setLoading(false);
-      setFilteredData(earthquakes);
-      console.log("data", filteredData);
+      setFilteredDataValue(earthquakes.length)
+      console.log("data", earthquakes);
     });
   }, []);
 
-  const handleFilterChange = (selectedValue: number) => {
-    setFilteredData(earthquakes.filter((earthquakes) => earthquakes.properties.mag >= selectedValue))
-  }
+  useEffect(() => {
+    if (selectedMagnitude === null) {
+      setFilteredData(earthquakes);
+    } else {
+      const filtered = earthquakes.filter((earthquake) => earthquake.properties.mag >= selectedMagnitude.value);
+      setFilteredDataValue(filtered.length)
+      setFilteredData(filtered);
+    }
+  }, [selectedMagnitude, earthquakes]);
+
 
   if (loading) {
     return (
@@ -67,11 +78,12 @@ export default function Home(props: EarthquakeListProps) {
   return (
     <main className="flex flex-col items-center h-screen md:container md:mx-auto">
       <div className="container">
-        <Filter onFilterChange={handleFilterChange}/>
+        <Filter setSelectedMagnitude={setSelectedMagnitude} />
       </div>
       <div className="container">
         <div className="w-full border-b my-10 border-gray-400"></div>
       </div>
+      <div className="container justify-start items-start italic mb-5 text-gray-500 text-sm">Listed below are {filteredDataValue} recent earthquakes</div>
       <div className="sm:w-full  lg:container grid grid-cols-3 gap-5 space-x-0 space-y-0 h-32">
         {filteredData.map((earthquakeLists) => (
           <div
